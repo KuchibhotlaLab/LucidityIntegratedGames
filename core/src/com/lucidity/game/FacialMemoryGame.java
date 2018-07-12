a1eec9378321ebbc30f75f91f42c0910b35d5178
Joey <joeywei75@gmail.com>
2018-07-12 12:45:54 -0400

    Grabbed picture names and tags from database for use in Game 2

--- core/src/com/lucidity/game/FacialMemoryGame.java
+++ core/src/com/lucidity/game/FacialMemoryGame.java
@@ -2,15 +2,21 @@ package com.lucidity.game;
 
 import com.badlogic.gdx.Game;
 
+import java.util.ArrayList;
+
 /**
  * Created by lixiaoyan on 7/3/18.
  */
 
 public class FacialMemoryGame extends Game {
     private String username;
+    private ArrayList<String> picturenames;
+    private ArrayList<ArrayList<String>> picturetags;
 
-    public FacialMemoryGame(String uname){
+    public FacialMemoryGame(String uname, ArrayList<String> pnames, ArrayList<ArrayList<String>> ptags){
         username = uname;
+        picturenames = pnames;
+        picturetags = ptags;
     }
 
     @Override
@@ -26,4 +32,7 @@ public class FacialMemoryGame extends Game {
         return username;
     }
 
+    public ArrayList<String> getPicturenames() { return picturenames; }
+
+    public ArrayList<ArrayList<String>> getPicturetags() { return picturetags; }
 }
