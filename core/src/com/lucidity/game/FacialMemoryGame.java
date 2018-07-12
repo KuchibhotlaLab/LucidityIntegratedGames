311a873e56eb6b3c6c8522d0a2b62aa4ec48219e
xli98 <xli152@jhu.edu>
2018-07-12 16:08:51 -0400

    Get date, gps locations, three booleans for under what condition the game is launched

--- core/src/com/lucidity/game/FacialMemoryGame.java
+++ core/src/com/lucidity/game/FacialMemoryGame.java
@@ -12,11 +12,21 @@ public class FacialMemoryGame extends Game {
     private String username;
     private ArrayList<String> picturenames;
     private ArrayList<ArrayList<String>> picturetags;
+    private boolean isLucid, isPatient, isCare;
+    private String dateTime;
+    private String coordinates;
 
-    public FacialMemoryGame(String uname, ArrayList<String> pnames, ArrayList<ArrayList<String>> ptags){
+    public FacialMemoryGame(String uname, ArrayList<String> pnames, ArrayList<ArrayList<String>> ptags,
+                            String date, String location, boolean lucid, boolean patient, boolean care){
         username = uname;
         picturenames = pnames;
         picturetags = ptags;
+        isLucid = lucid;
+        isPatient = patient;
+        isCare = care;
+        dateTime = date;
+        coordinates = location;
+
     }
 
     @Override
@@ -35,4 +45,10 @@ public class FacialMemoryGame extends Game {
     public ArrayList<String> getPicturenames() { return picturenames; }
 
     public ArrayList<ArrayList<String>> getPicturetags() { return picturetags; }
+
+    public boolean getLucid(){return isLucid;}
+    public boolean getPatient(){return isPatient;}
+    public boolean getCare(){return isCare;}
+    public String getDateTime(){return dateTime;}
+    private String getLocations(){return coordinates;}
 }
--- core/src/com/lucidity/game/WorkingMemoryGame.java
+++ core/src/com/lucidity/game/WorkingMemoryGame.java
@@ -4,12 +4,18 @@ import com.badlogic.gdx.Game;
 
 public class WorkingMemoryGame extends Game {
     private String username;
+    private boolean isLucid, isPatient, isCare;
     private String currentDateTime;
+    private String coordinates;
 
-    public WorkingMemoryGame(String uname, String date){
+    public WorkingMemoryGame(String uname, String date, String location, boolean lucid, boolean patient, boolean care){
+        isLucid = lucid;
+        isPatient = patient;
+        isCare = care;
 
     	username = uname;
     	currentDateTime = date;
+        coordinates = location;
     }
 
 	@Override
@@ -28,5 +34,9 @@ public class WorkingMemoryGame extends Game {
     public String getUsername() {
         return username;
     }
-    public String getDate() {return currentDateTime;}
+    public String getDateTime() {return currentDateTime;}
+    public boolean getLucid(){return isLucid;}
+    public boolean getPatient(){return isPatient;}
+    public boolean getCare(){return isCare;}
+    public String getLocation(){return coordinates;}
 }
