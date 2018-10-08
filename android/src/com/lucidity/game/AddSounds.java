ebee234abfcaacab86ddfe79a1f2396c36251420
xli98 <xli152@jhu.edu>
2018-10-08 13:22:57 -0400

    Added placeholder for implementing sounds and music incorporation

---
+++ android/src/com/lucidity/game/AddSounds.java
@@ -0,0 +1,16 @@
+package com.lucidity.game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSounds extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_sounds);
+    }
+     /*TODO: implement this
+    //https://developer.android.com/guide/topics/media/mediaplayer
+    https://stackoverflow.com/questions/11982195/how-to-access-music-files-from-android-programatically*/
+}
