b5b6ed240b9c50d0787a6ce26c045b73c47cf5d3
xli98 <xli152@jhu.edu>
2018-11-01 17:48:07 -0400

    Attempted spotify URI

--- android/src/com/lucidity/game/AddSpotify.java
+++ android/src/com/lucidity/game/AddSpotify.java
@@ -1,10 +1,16 @@
 package com.lucidity.game;
 
+import android.app.SearchManager;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
@@ -12,6 +18,8 @@ import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
+
+import java.util.List;
 
 
 /* TODO:
@@ -30,17 +38,18 @@ https://stackoverflow.com/questions/12698428/starting-a-song-from-spotify-intent
 //TODO NOTICE: spotify is registered for non-monetary personal project permission
 public class AddSpotify extends AppCompatActivity {
     private static final String CLIENT_ID = "ab466596aaa041d89f062bc0845a16f0";
-    private static final String REDIRECT_URI = "http://localhost:8888/callback";
+    //private static final String REDIRECT_URI = "http://localhost:8888/callback";
+    private static final String REDIRECT_URI = "http://ec2-174-129-156-45.compute-1.amazonaws.com/lucidity/callback";
     private SpotifyAppRemote mSpotifyAppRemote;
     public static final int AUTH_TOKEN_REQUEST_CODE = 0x10;
     public static final int AUTH_CODE_REQUEST_CODE = 0x11;
+    private static final int REQUEST_CODE = 1337;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_spotify);
-
     }
 
     @Override
@@ -54,6 +63,7 @@ public class AddSpotify extends AppCompatActivity {
                         .setRedirectUri(REDIRECT_URI)
                         .showAuthView(true)
                         .build();
+        System.out.println("authorized, theoretically");
 
         //
         SpotifyAppRemote.connect(this, connectionParams,
@@ -62,8 +72,7 @@ public class AddSpotify extends AppCompatActivity {
                     @Override
                     public void onConnected(SpotifyAppRemote spotifyAppRemote) {
                         mSpotifyAppRemote = spotifyAppRemote;
-                        //Log.d("MainActivity", "Connected! Yay!");
-                        System.out.println("Add spotify successfully!");
+                        System.out.println("Connected spotify successfully!");
 
                         // Now you can start interacting with App Remote
                         connected();
@@ -71,8 +80,8 @@ public class AddSpotify extends AppCompatActivity {
 
                     @Override
                     public void onFailure(Throwable throwable) {
-                        //Log.e("MainActivity", throwable.getMessage(), throwable);
-                        System.out.println("Error error error");
+                        Log.e("Error error error", throwable.getMessage(), throwable);
+                        System.out.println("Error not authenticated " + throwable.getMessage());
                         // Something went wrong when attempting to connect! Handle errors here
                     }
                 });
@@ -93,6 +102,10 @@ public class AddSpotify extends AppCompatActivity {
                     Uri.parse("android-app://" + this.getPackageName()));
             startActivity(intent);
         }*/
+        //playPlayMusic();
+        playSearchSong("skyfall");
+
+
     }
 
     private void connected() {
@@ -103,5 +116,31 @@ public class AddSpotify extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
+
+    private void playPlayMusic() {
+        Intent i = new Intent(Intent.ACTION_MEDIA_BUTTON);
+        i.setComponent(new ComponentName("com.spotify.music", "com.spotify.music.internal.receiver.MediaButtonReceiver"));
+        i.putExtra(Intent.EXTRA_KEY_EVENT,new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PLAY));
+        sendOrderedBroadcast(i, null);
+
+        i = new Intent(Intent.ACTION_MEDIA_BUTTON);
+        i.setComponent(new ComponentName("com.spotify.music", "com.spotify.music.internal.receiver.MediaButtonReceiver"));
+        i.putExtra(Intent.EXTRA_KEY_EVENT,new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_PLAY));
+        sendOrderedBroadcast(i, null);
+    }
+
+    public void playSearchSong(String title) {
+        Intent intent = new Intent(MediaStore.INTENT_ACTION_MEDIA_PLAY_FROM_SEARCH);
+        intent.setComponent(new ComponentName("com.spotify.music", "com.spotify.music.MainActivity"));
+        intent.putExtra(MediaStore.EXTRA_MEDIA_FOCUS, MediaStore.Audio.Artists.ENTRY_CONTENT_TYPE);
+        //intent.putExtra(MediaStore.EXTRA_MEDIA_ARTIST, artist);
+        //intent.putExtra(SearchManager.QUERY, artist);
+        intent.putExtra(MediaStore.EXTRA_MEDIA_TITLE, title);
+        intent.putExtra(SearchManager.QUERY, title);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
